de7def39cee9ca15eaa24c49993249ff297d81a8
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-10-21 23:36:16 -0400

    purchase and inventory functions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const {table} = require("table");
+const {
+  table
+} = require("table");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -11,25 +13,56 @@ const db = mysql.createConnection({
 });
 
 const displayStock = () => {
-  
-  db.query("SELECT * FROM products", function (err, result) {
+
+  db.query("SELECT * FROM products", function (err, inventory) {
     if (err) throw err;
-    let data = [["ID", "Product", "Department", "Price ($)", "Inventory"]];
-    
-    result.forEach(product => {
+    let data = [
+      ["ID", "Product", "Department", "Price ($)", "Inventory"]
+    ];
+
+    inventory.forEach(product => {
       let col = [];
       col.push(product.id, product.product_name, product.department_name, product.price, product.stock_quantity);
       data.push(col);
     })
     let output = table(data);
     console.log(output);
+    purchaseById();
+  })
+}
+
+const purchaseById = () => {
+
+  inquirer.prompt([{
+    type: "input",
+    name: "id",
+    message: "Please enter the ID of the item you would like to purchase:",
+    default: "1",
+    validate: (answer) => (!isNaN(answer)) ? true : false
+  }]).then(purchase => {
+    checkInventory(purchase);
   });
+}
+
+const checkInventory = (purchase) => {
+  db.query("SELECT * FROM products WHERE id = ?", purchase.id, (err, item) => {
+    if (err) throw err;
+    if (item === []) {
+      console.log("Sorry, that item does not exist in our inventory.");
+      purchaseById();
+    } else {
+      if (item.stock_quantity === 0) {
+        console.log("Sorry, that itme is out of stock.")
+      } else {
+        updateInventory();
+      }
+    }
+  })
 }
 
 db.connect(err => {
 
   if (err) throw err;
-  console.log(`Connected on ${db.threadId}.`);
+  console.log("Connected to database.");
   displayStock();
-  // userMenu(); 
 });
